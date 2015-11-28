ed67edbbe9f2e4d6355b75d74ff0c51dc5ee18bd
steveb <ste.bossi@gmail.com>
2015-11-28 22:08:45 +0100

    refactoring taxoffice, rimozione tasse finte per l'esenzione

--- src/main/java/TaxOffice.java
+++ src/main/java/TaxOffice.java
@@ -5,67 +5,29 @@ import java.util.*;
 import src.main.java.salestaxes.*;
 
 public class TaxOffice implements TaxRepository {
-    private List<Match> taxMatches = new ArrayList<>();
-
-    public TaxOffice(){
-        Tax tax = new Tax();
-        taxMatches.add(new DescriptionMatch(tax.exemption(), tax.basic(), "book", "chocolate", "pills"));
-        taxMatches.add(new ImportedMatch(tax.duty(), tax.dutyFree()));
-    }
+    private Tax tax = new Tax();
+    private String[] exemptions = {"book", "chocolate", "pills"};
 
     public Set<TaxRule> taxesFor(Sellable product){
         Set<TaxRule> rules = new HashSet<>();
-        for (Match match: taxMatches){
-            rules.add(match.assignRule(product));
+        if (isNotExempt(product)){
+            rules.add(tax.basic());
         }
+        if (product.imported()){
+            rules.add(tax.duty());
+        } 
         return rules;
     }
-}
 
-
-interface Match {
-    public TaxRule assignRule(Sellable product);
-}
-
-
-class DescriptionMatch implements Match{
-    String[] keys;
-    TaxRule rule;
-    TaxRule otherwise;
-
-    public DescriptionMatch(TaxRule rule, TaxRule otherwise, String... keys){
-        this.keys = keys;
-        this.rule = rule;
-        this.otherwise = otherwise;
+    private boolean isNotExempt(Sellable product){
+        return !isExempt(product);
     }
-
-    public TaxRule assignRule(Sellable product){
-        if (test(product.description())) return rule;
-        return otherwise;
-    }
-
-    private boolean test(String description){
-        for (String key: keys){
-            if (description.contains(key)){
+    private boolean isExempt(Sellable product){
+        for (String key: exemptions){
+            if (product.description().contains(key)){
                 return true;
             };
         }
         return false;
-    }
-}
-
-
-class ImportedMatch implements Match{
-    TaxRule rule;
-    TaxRule otherwise;
-
-    public ImportedMatch (TaxRule rule, TaxRule otherwise){
-        this.rule = rule;
-        this.otherwise = otherwise;
-    }
-
-    public TaxRule assignRule(Sellable product){
-        if (product.imported()) return rule;
-        return otherwise;
     }
 }
